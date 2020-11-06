f5211a1e13fb6e88f647ff3c4220ed76f39e630e
xie302 <42813327+xie302@users.noreply.github.com>
2020-11-06 15:22:28 -0500

    Stage 2; Basic login/signup; Basic UI

--- app.py
+++ app.py
@@ -6,20 +6,21 @@ db = mysql.connector.connect(
     host="34.72.99.39",
     user="root",
     password="CS348",
-    # ssl_ca= '/etc/mysql/ssl/client/ca-cert.pem',
     database="CS348Proj"
 )
+
 app = Flask(__name__)
 app.secret_key = "This is a secret string"
+
 app.config['MYSQL_DATABASE_USER'] = 'newuser'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'awesomeblog'
 app.config['MYSQL_DATABASE_DB'] = 'hosportal'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
 app.permanent_session_lifetime = timedelta(minutes=10)
 
 
-
-@app.route("/")
+@app.route("/home")
 def home():
     return render_template("index.html")
 
@@ -36,7 +37,7 @@ def signup():
         cur.execute("INSERT INTO hosuser(username, email, password, type) VALUES (%s, %s, %s, %s)", (username, email, password, type))
         db.commit()
         cur.close()
-        db.close()
+        flash("User " + username + " created!")
         return redirect(url_for("login"))
 
     else:
@@ -48,10 +49,24 @@ def signup():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        session.permanent = True
-        user = request.form["username"]
-        session["user"] = user
-        return redirect(url_for("user"))
+        username = request.form["username"]
+        pw = request.form["password"]
+        cur = db.cursor()
+        query = "SELECT username, email, password, type from hosuser where username = '" + username + "'"
+        cur.execute(query)
+        usr = cur.fetchone()
+        cur.close()
+        if usr:
+            if pw == usr[2]:
+                session.permanent = True
+                session["user"] = usr
+                return redirect(url_for("user"))
+            else:
+                flash("Wrong Password!")
+                return render_template("login.html")
+        else:
+            flash("Invalid User!")
+            return render_template("login.html")
     else:
         if "user" in session:
             return redirect(url_for("user"))
@@ -62,8 +77,39 @@ def login():
 def user():
     if "user" in session:
         user = session["user"]
-        return render_template("index.html", content=user)
+        return render_template("index.html", role=user[3], content=user[0])
     else:
+        flash("You are not logged in!")
+        return redirect(url_for("login"))
+
+
+@app.route("/profile")
+def profile():
+    if "user" in session:
+        user = session["user"]
+        return render_template("profile.html", role=user[3], content=user[0])
+    else:
+        flash("You are not logged in!")
+        return redirect(url_for("login"))
+
+
+@app.route("/appointment")
+def appointment():
+    if "user" in session:
+        user = session["user"]
+        return render_template("Appointment.html", role=user[3], content=user[0])
+    else:
+        flash("You are not logged in!")
+        return redirect(url_for("login"))
+
+
+@app.route("/prescription")
+def prescription():
+    if "user" in session:
+        user = session["user"]
+        return render_template("Prescription.html", role=user[3], content=user[0])
+    else:
+        flash("You are not logged in!")
         return redirect(url_for("login"))
 
 
@@ -71,6 +117,24 @@ def user():
 def logout():
     session.pop("user", None)
     return redirect(url_for("login"))
+
+
+@app.route("/delete")
+def delete():
+    if "user" in session:
+        user = session["user"]
+        cur = db.cursor()
+        print(user[0])
+        sql = "DELETE FROM hosuser WHERE username = '" + user[0] + "'"
+        cur.execute(sql)
+        db.commit()
+        cur.close()
+        session.pop("user", None)
+        flash("User Deleted!")
+        return redirect(url_for("login"))
+    else:
+        flash("You are not logged in!")
+        return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
