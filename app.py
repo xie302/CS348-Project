8732015fd3b147c5b6791186b9587a132866d94c
unknown <xu1210@purdue.edu>
2020-12-08 22:11:31 +0800

    prescription done

--- app.py
+++ app.py
@@ -105,43 +105,98 @@ def appointment():
 
 @app.route("/prescription", methods=["POST", "GET"])
 def prescription():
-    cur = db.cursor(buffered=True)
-    cur.execute("SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;")
-    cur.execute("START TRANSACTION;")
-    resultRows = cur.execute("SELECT * FROM Prescription;")
-    preDetails = cur.fetchall()
-    return render_template("Prescription.html", preDetails=preDetails)
-    #button redirect to /pre_select
+    if "user" in session:
+        user = session["user"]
+        user_type = user[3]
+        user_id = user[4]
+        print(user_type)
+        if user_type == 'doctor' or user_type == 'Doctor':
+            cur = db.cursor(buffered=True)
+            cur.execute("SELECT Doctor_id FROM Doctor WHERE UID = " + user_id + ";")
+            doc_result = cur.fetchall()
+            doc_id = str(doc_result[0][0])
+            cur.execute("SELECT Pre_id, Pre_name, Pre_amount, Patient_id, Name"
+                        " FROM Report NATURAL JOIN Prescription NATURAL JOIN Appointment NATURAL JOIN Patient"
+                        " WHERE Doctor_id = " + doc_id + ";")
+            pat_pre_details = cur.fetchall()
+            db.commit()
+            cur.execute("SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;")
+            cur.execute("START TRANSACTION;")
+            cur.execute("SELECT * FROM Prescription;")
+            preDetails = cur.fetchall()
+            return render_template("Prescription_doctor.html", preDetails=preDetails, pat_pre_details=pat_pre_details)
+
+        elif user_type == 'patient' or user_type == 'Patient':
+            cur = db.cursor(buffered=True)
+            cur.execute("SELECT Patient_id FROM Patient WHERE UID = " + user_id + ";")
+            pat_result = cur.fetchall()
+            pat_id = str(pat_result[0][0])
+            cur.execute("SELECT Pre_id, Pre_name, Pre_amount"
+                        " FROM Report NATURAL JOIN Prescription"
+                        " WHERE Report_num IN (SELECT A.Report_num"
+                        " FROM Appointment A"
+                        " WHERE A.Patient_id =" + pat_id + ");")
+            preDetails = cur.fetchall()
+            return render_template("Prescription_patient.html", preDetails=preDetails)
+            #button redirect to /pre_select
+        else:
+            return("unknown user")
+            return render_template("login.html", role=user[3], content=user[0])
+    else:
+        flash("You are not logged in!")
+        return redirect(url_for("login"))
 
 
 @app.route("/pre_select", methods=["POST", "GET"])
 def pre_select():
-    if request.method == 'POST':
-        preDetails = request.form
-        pre_id = preDetails['pre_id']
-        amount_toassign = preDetails['amount']
-        pat_id = preDetails['pat_id']
-        doc_id = preDetails['doc_id']
-        cur = db.cursor()
-        cur.execute("SELECT amount_left FROM Prescription WHERE Pre_id = %s;", pre_id)
-        amount_left = cur.fetchall()
-        if amount_toassign <= amount_left[0]:
-            cur.execute("UPDATE Prescription"
-                        "SET amount_left = amount_left - %s"
-                        "WHERE Pre_id = %s;", (amount_toassign, pre_id))
-            cur.execute("UPDATE Report"
-                        "SET Report.Pre_id = %s"
-                        "WHERE Report.Report_num = (SELECT MAX(Appointment.Report_num)"
-                        "FROM Appointment"
-                        "WHERE Appointment.Patient_id = %s AND Appointment.Doctor_id = %s);", (pre_id, pat_id, doc_id))
-            cur.execute("UPDATE Doctor SET Doctor.state = 'available' WHERE Doctor_id = %s;", (doc_id))
-            db.commit()
-            return render_template("Pre_select.html")
+    if "user" in session:
+        user = session["user"]
+        if request.method == 'POST':
+            #get pre_id, amount_toassign, pat_id
+            preDetails = request.form
+            pre_id = preDetails['pre_id']
+            amount_toassign = int(preDetails['amount'])
+            pat_id = preDetails['pat_id']
+            cur = db.cursor()
+            #get doc_id from user
+            cur.execute("SELECT Doctor_id FROM Doctor WHERE UID = " + user[4] + ";")
+            doc_result = cur.fetchall()
+            doc_id = str(doc_result[0][0])
+            #get amount_left
+            query = "SELECT amount_left FROM Prescription WHERE Pre_id =" + pre_id + ";"
+            cur.execute(query)
+            result = cur.fetchall()
+            amount_left = result[0][0]
+            if amount_toassign <= amount_left:
+                query = ("UPDATE Prescription"
+                         " SET amount_left = amount_left - " + str(amount_toassign) + ""
+                         " WHERE Pre_id = " + pre_id + ";")
+                cur.execute(query)
+                query = ("UPDATE Report"
+                         " SET Report.Pre_id =" + pre_id + ""
+                         " WHERE Report.Report_num = (SELECT MAX(Appointment.Report_num)"
+                         " FROM Appointment"
+                         " WHERE Appointment.Patient_id =" + pat_id + " AND Appointment.Doctor_id =" + doc_id + ");")
+                cur.execute(query)
+                query = ("UPDATE Report"
+                         " SET Report.Pre_amount =" + str(amount_toassign) + ""
+                         " WHERE Report.Report_num = (SELECT MAX(Appointment.Report_num)"
+                         " FROM Appointment"
+                         " WHERE Appointment.Patient_id =" + pat_id + " AND Appointment.Doctor_id =" + doc_id + ");")
+                cur.execute(query)
+                cur.execute("UPDATE Doctor SET Doctor.state = 'available' WHERE Doctor_id =" + doc_id + ";")
+                db.commit()
+                # need to be shown as small side titles
+                return("Prescription request successful")
+            else:
+                db.commit()
+                return ("Prescription request unsuccessful")
         else:
-            db.commit()
-            return render_template("Pre_select.html")
+            return render_template("pre_select.html")
     else:
-        return render_template("Pre_select.html")
+        flash("You are not logged in!")
+        return redirect(url_for("login"))
+
 
 
 @app.route("/logout")
